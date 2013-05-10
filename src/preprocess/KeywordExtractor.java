feec3f2df18b573c46477791fb78c3add2b171e3
Yicheng Liu <liuchen250.student@gmail.com>
2013-05-10 16:53:45 +0800

    simple word count

--- src/preprocess/KeywordExtractor.java
+++ src/preprocess/KeywordExtractor.java
@@ -1,18 +1,54 @@
 package preprocess;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.ansj.domain.Term;
 import org.ansj.splitWord.analysis.ToAnalysis;
 import org.ansj.util.recognition.NatureRecognition;
+import org.apache.commons.collections.bag.TreeBag;
+import org.apache.commons.io.FileUtils;
 
 public class KeywordExtractor {
-    public static void main(String[] args) {
-        String content = "玉渊潭公园，人不算特别多，但节日气氛相当浓！静观儿子快乐大滑梯，也祝各位朋友，中秋节快乐！长假快乐";
-        List<Term> terms = ToAnalysis.paser(content);
-        new NatureRecognition(terms).recognition() ;
-        for (Term term : terms) {
-            System.out.println(term.getName()+"\t"+term.getNatrue().natureStr);
+    TreeBag wordBag = new TreeBag();
+    ArrayList<String> wordList = new ArrayList<String>();
+
+    public void countWords() {
+        File weiboList= new File("/home/gods/Downloads/玉渊潭公园.txt");
+        try {
+            List<String> lines = FileUtils.readLines(weiboList);
+            for (String line : lines) {
+                List<Term> terms = ToAnalysis.paser(line);
+                new NatureRecognition(terms).recognition() ;
+                for (Term term : terms) {
+                    //System.out.print(term.getName()+"/"+term.getNatrue().natureStr + "  ");
+                    if(wordBag.add(term.getName())) {
+                        wordList.add(term.getName());
+                    }
+                }
+                //System.out.println();
+            }
+            Collections.sort(wordList, new Comparator<String>() {
+
+                @Override
+                public int compare(String o1, String o2) {
+                    return Integer.compare(wordBag.getCount(o1), wordBag.getCount(o2));
+                }
+
+            });
+            for (Object term : wordList) {
+                System.out.println((String)term + " " + wordBag.getCount(term));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        (new KeywordExtractor()).countWords();
     }
 }
