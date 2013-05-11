3ec28990c3bdcfcb69e666e6c77f4baa816a7209
Yicheng Liu <liuchen250.student@gmail.com>
2013-05-11 16:18:56 +0800

    simple opinion miner

--- src/preprocess/WeiboProcessor.java
+++ src/preprocess/WeiboProcessor.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.ansj.domain.Term;
 import org.ansj.splitWord.analysis.ToAnalysis;
@@ -13,15 +14,20 @@ import org.ansj.util.recognition.NatureRecognition;
 import org.apache.commons.collections.bag.TreeBag;
 import org.apache.commons.io.FileUtils;
 
-public class KeywordExtractor {
+public class WeiboProcessor {
     TreeBag wordBag = new TreeBag();
     ArrayList<String> wordList = new ArrayList<String>();
+    Pattern personPattern = Pattern.compile("(.*人(很|好|太|相当|特别|超级|比较)*多.*)|(.*人山人海.*)");
+    Pattern personFewPattern = Pattern.compile("(.*人(不|不算)(很|好|太|相当|特别|超级|比较)*多.*)|(.*人(很|好|太|相当|特别|超级|比较)*少.*)");
 
     public void countWords() {
         File weiboList= new File("/home/gods/Downloads/玉渊潭公园.txt");
         try {
             List<String> lines = FileUtils.readLines(weiboList);
             for (String line : lines) {
+                if (personPattern.matcher(line).matches()) {
+                    System.out.println(line);
+                }
                 List<Term> terms = ToAnalysis.paser(line);
                 new NatureRecognition(terms).recognition() ;
                 for (Term term : terms) {
@@ -41,7 +47,7 @@ public class KeywordExtractor {
 
             });
             for (Object term : wordList) {
-                System.out.println((String)term + " " + wordBag.getCount(term));
+                //System.out.println((String)term + " " + wordBag.getCount(term));
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -49,6 +55,6 @@ public class KeywordExtractor {
     }
 
     public static void main(String[] args) {
-        (new KeywordExtractor()).countWords();
+        (new WeiboProcessor()).countWords();
     }
 }
